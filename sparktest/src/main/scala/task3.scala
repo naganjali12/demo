1e4a201288bdb570a6b3522840fd72a2e8b8c460
Naganjali Kamjula(UST <216784@ust-global.com>
2023-03-06 14:06:16 +0530

    case4 commit

--- sparktest/src/main/scala/task3.scala
+++ sparktest/src/main/scala/task3.scala
@@ -20,12 +20,25 @@ object task3 {
     df1.createOrReplaceTempView("claims_revision_table")
     //df1.show()
 
-//    val modifiedClaims = spark.sql("SELECT Claim, Line, Date, Line_amt, FND_CD, ROW_NUMBER() OVER (PARTITION BY Claim,Line ORDER BY Date DESC) AS Version, CASE WHEN ROW_NUMBER() OVER (PARTITION BY Claim, Line ORDER BY Date DESC) = 1 THEN 'Y' ELSE 'N' END AS Current_Version_Flag FROM claims_table")
-//
-//    modifiedClaims.createOrReplaceTempView("output_table")
-//    modifiedClaims.show()
+    val modifiedClaims = spark.sql("SELECT Claim, Line, Date, Line_amt, FND_CD FROM claims_table")
 
-    spark.sql("CREATE TABLE output_table (Claim string,  Line int,  Date date,  Line_amt double,  FND_CD string,  Version int, Current_Version_Flag string)USING PARQUET;")
+    modifiedClaims.createOrReplaceTempView("output_table")
+    modifiedClaims.show()
+
+   spark.sql("""SELECT c.Claim, c.Line, c.Date, c.Line_amt, c.FND_CD,
+  ROW_NUMBER() OVER (PARTITION BY C.Claim, C.Line ORDER BY COALESCE(CR.Revion_Order, 0) DESC)  AS Version,
+  CASE WHEN cr.Revion_Order = MAX(cr.Revion_Order) OVER (PARTITION BY c.Claim, c.Line) THEN 'Y' ELSE 'N' END AS Current_Version_flag
+FROM claims_table c
+LEFT JOIN claims_revision_table cr
+  ON c.Claim = cr.Claim AND c.Line = cr.Line
+UNION ALL
+SELECT cr.Claim, cr.Line, cr.Date, cr.Line_amt, cr.FND_CD,
+  ROW_NUMBER() OVER (PARTITION BY CR.Claim, CR.Line ORDER BY CR.Revion_Order DESC) + 1 AS Version,
+  CASE WHEN CR.Revion_Order = MAX(CR.Revion_Order) OVER (PARTITION BY cr.Claim, cr.Line) THEN 'Y' ELSE 'N' END AS Current_Version_flag
+FROM claims_revision_table cr
+ORDER BY Claim, Line, Version;
+
+""").show()
 
   }
 }
